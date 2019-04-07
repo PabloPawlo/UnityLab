9c4a2eec1f3c77a0d12974b4df06a0a2c5d90571
PabloPawlo <pawel.pawlica95@gmail.com>
2019-04-07 19:23:34 +0200

    Game

---
+++ Assets/FTPInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class FTPInput : MonoBehaviour {
+
+    private CharacterController _characterController;
+    public float speed = 15.0f;
+    public float gravity = -9.81f;
+
+
+	// Use this for initialization
+	void Start () {
+        _characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaY = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaY);
+
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _characterController.Move(movement);
+
+
+
+
+
+    }
+}
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public enum RotationAxes
+    {
+        MouseXandY=0,
+        MouseX=1,
+        MouseY=2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    public float sensitivityX = 9.0f, sensitivityY = 9.0f;
+    public float minimumVert = -15.0f, maximumVert = 15.0f;
+
+    private float _rotationX = 0, _rotationY = 0;
+    private Camera cameraPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+        cameraPlayer = GameObject.Find("camera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(axes==RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityY;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            transform.localEulerAngles = new Vector3(_rotationX, transform.localEulerAngles.y, 0);
+        }
+        else
+        {
+            _rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityY;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            transform.localEulerAngles = new Vector3(0, _rotationY, 0);
+            cameraPlayer.transform.localEulerAngles = new Vector3(_rotationX, 0, 0);
+        }
+	}
+}
